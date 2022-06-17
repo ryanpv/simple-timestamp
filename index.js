9284b2b98e66e6c9873950ff080c29f5a24f0570
Ryan <ryanpvoong@gmail.com>
2022-06-17 15:18:17 -0400

    initial commit with unfinished product

--- index.js
+++ index.js
@@ -20,13 +20,44 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+app.get("/api/timestamp", function (req, res) {
+  let date = new Date();
+  res.json({ unix: Date.now(), utc: date.toISOString() });
+})
+
+app.get("/api/timestamp/:timestamp", function (req, res) {
+  const { timestamp } = req.params
+  // const date = new Date(timestamp).toUTCString()
+  // if(timestamp.match(/\d{5,}/)) {
+  //   let tsnum = timestamp
+  //   return num.toString()
+  // }
+  const date = new Date(timestamp)
+
+  if(date.toUTCString() == "Invalid Date"){
+    res.json({current: date.toUTCString()})
+  } else {
+  console.log(date);
+
+  res.json({ unix: date.valueOf(), utc: date.toUTCString() });}
 });
+
+// 978325200000
+
+// app.get("/api/timestamp/:unixDate", function (req, res) {
+//   const unixDate = req.params.unixDate
+//   const date = new Date();
+//   const unixTs = date.valueOf(unixDate)
+//   res.json({ utc: date.toUTCString() });
+// })
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+
+const port = process.env.PORT || 5959;
+
+app.listen(port, () => {
+    console.log('listening on port...' + port);
 });
